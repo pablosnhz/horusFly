4bfdf29563edc01f85333b6348c81d9edba7e526
PabloSnhz <pablosanhuezaaa@gmail.com>
2025-03-22 21:27:37 -0300

    card profile with details of reserve

--- appHorusFly/src/app/routes/auth/pages/profile/profile.component.ts
+++ appHorusFly/src/app/routes/auth/pages/profile/profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  reservationData: any = null;
 
+  ngOnInit(): void {
+    this.getReservationData();
+  }
+
+  getReservationData() {
+    const storedData = sessionStorage.getItem('form');
+    if (storedData) {
+      this.reservationData = JSON.parse(storedData);
+      console.log(this.reservationData);
+    }
+  }
 }
