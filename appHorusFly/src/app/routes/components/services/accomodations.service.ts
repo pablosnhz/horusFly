f88bfe1385d51d69348b9b1f8b2cefdf4469c8c1
PabloSnhz <pablosanhuezaaa@gmail.com>
2025-04-05 23:32:39 -0300

    applying filters for hotel components

--- appHorusFly/src/app/routes/components/services/accomodations.service.ts
+++ appHorusFly/src/app/routes/components/services/accomodations.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, signal, WritableSignal } from '@angular/core';
-import { delay, finalize, Observable } from 'rxjs';
+import { BehaviorSubject, delay, finalize, map, Observable, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -8,14 +8,44 @@ import { environment } from 'src/environments/environment';
 })
 export class AccomodationsService {
   $loading: WritableSignal<boolean> = signal(false);
+  private filterSubject = new BehaviorSubject<any>({});
+  filter$ = this.filterSubject.asObservable();
+
+  allFlights: any[] = [];
 
   constructor(private http: HttpClient) {}
 
-  getInfo(): Observable<any> {
+  getInfoHotel(): Observable<any> {
     this.$loading.set(true);
     return this.http.get<any>(`${environment.apiEndpoints.url}api/hotel/list`).pipe(
-      // delay(3000),
+      tap((response) => {
+        this.allFlights = response;
+      }),
       finalize(() => this.$loading.set(false)),
+    );
+  }
+
+  updateFilter(filters: any) {
+    this.filterSubject.next(filters);
+  }
+
+  filterHotel$(filters: any): Observable<any[]> {
+    return this.getInfoHotel().pipe(
+      map((data) => {
+        const hotel = Array.isArray(data) ? data : data.value || [];
+        return hotel.filter((place: any) => {
+          const placeCheck =
+            !filters.place || place.city?.toLowerCase().includes(filters.place.toLowerCase());
+          const priceCheck =
+            !filters.price ||
+            (filters.price === '1' && place.price <= 100) ||
+            (filters.price === '2' && place.price > 100 && place.price <= 200) ||
+            (filters.price === '3' && place.price > 300);
+          const ratingCheck = !filters.rating || place.rating == filters.rating;
+
+          return placeCheck && priceCheck && ratingCheck;
+        });
+      }),
     );
   }
 }
